06f6d82a8e079a8ae46264a87c0a2136b31d8afb
Keyance <klara.konopacova@seznam.cz>
2023-11-04 18:25:45 +0100

    Přidejte soubory projektu.

---
+++ L4UkolPocetZnaku/Program.cs
@@ -0,0 +1,22 @@
+﻿using L4UkolPocetZnaku;
+
+List <ZnakPocet> vyskyty = new List <ZnakPocet> ();
+
+string text = Console.ReadLine ();
+
+foreach (char a in text)
+{
+    foreach (ZnakPocet b in vyskyty) {
+        if (char.ToLower(b.znak) == char.ToLower(a))
+        {
+            b.vyskyt++;
+        }
+        else
+            vyskyty.Add(new ZnakPocet { znak = a, vyskyt = 1 });
+            }
+}
+
+foreach (ZnakPocet c in vyskyty)
+{
+    Console.WriteLine($"Znak: {c.znak}, Výskyt: {c.vyskyt}");
+}
---
+++ L4UkolPocetZnaku/ZnakPocet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4UkolPocetZnaku
+{
+    public class ZnakPocet
+    {
+        public int vyskyt = 0;
+        public char znak;
+
+    }
+}
